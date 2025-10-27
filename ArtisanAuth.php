69f603056ff22767d87f8d24f82e289c0af15321
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-27 23:49:59 +0100

    Implement ArtisanAuth class and createArtisan method for artisan registration

---
+++ ArtisanAuth.php
@@ -0,0 +1,27 @@
+<?php
+    require('Config.php');
+    class ArtisanAuth extends Config {
+        public function createArtisan($first_name, $last_name, $email, $password) {
+            $query = "INSERT INTO `artisan_tb`(`first_name`, `last_name`, `email`, `password`) VALUES('$first_name', '$last_name', '$email', '$password')";
+            $saveArtisan = mysqli_query($this->connection, $query);
+            if($saveArtisan) {
+                echo 'artsan sign up successful';
+                // return ;
+            } else {
+                echo 'artisan creation failed';
+            }
+        }
+    }
+
+    // Receive from frontend
+    $input = file_get_contents('php://input');
+    $artisanDetails = json_decode($input); // COnverts the details into what PHP understands
+    $first_name = $artisanDetails->first_name;
+    $last_name = $artisanDetails->last_name;
+    $email = $artisanDetails->email;
+    $password = $artisanDetails->password;
+
+    $auth = new ArtisanAuth;
+    // $auth->createArtisan($first_name, $last_name, $email, $password);
+    $auth->createArtisan('Femi', 'John', 'fm2@gmail.com', '123456')
+?>
