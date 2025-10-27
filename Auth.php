c883dedc2068737b67f7570f5972d80176fb30d8
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-27 09:09:00 +0100

    Implement SQL query in createCustomer method of Auth class

--- Auth.php
+++ Auth.php
@@ -2,7 +2,7 @@
     require('Config.php');
     class Auth extends Config {
         public function createCustomer() {
-            
+            "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`)";
         }
     }
 ?>
