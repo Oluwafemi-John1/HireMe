fdcc0c59c2d957f94ac54a8eefec2afd339ff3ed
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-27 09:18:25 +0100

    Fix createCustomer method to properly execute SQL query and handle success/failure messages

--- Auth.php
+++ Auth.php
@@ -2,7 +2,14 @@
     require('Config.php');
     class Auth extends Config {
         public function createCustomer($first_name, $last_name, $email, $password) {
-            "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`) VALUES($first_name, $last_name, $email, $password)";
+            $query = "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`) VALUES($first_name, $last_name, $email, $password)";
+            $saveCustomer = mysqli_query($this->connection, $query);
+            if($saveCustomer) {
+                echo 'customer sign up successful';
+                // return ;
+            } else {
+                echo 'customer creation failed';
+            }
         }
     }
 ?>
