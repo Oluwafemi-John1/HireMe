6ba64a5c4f526cc8f2d54034d770e28ed96dd259
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-27 08:33:44 +0100

    Refactor database connection method to use constructor for initialization

--- Config.php
+++ Config.php
@@ -5,9 +5,10 @@
         protected $username = 'root';
         protected $password = '';
         protected $database = 'hire_me';
+        protected $connection;
 
-        protected function connectToDB() {
-            mysqli_connect($this->host, $this->username, $this->password, $this->database);
+        protected function __construct() {
+            $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
         }
     }
 ?>
