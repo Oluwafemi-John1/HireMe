58751dbabb24da20e2bd776d718a23c388fb4acf
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-27 09:13:03 +0100

    Update createCustomer method to accept parameters for customer data insertion

--- Auth.php
+++ Auth.php
@@ -1,8 +1,8 @@
 <?php
     require('Config.php');
     class Auth extends Config {
-        public function createCustomer() {
-            "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`)";
+        public function createCustomer($first_name, $last_name, $email, $password) {
+            "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`) VALUES($first_name, $last_name, $email, $password)";
         }
     }
 ?>
