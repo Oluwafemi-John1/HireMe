147f0fd356f4bc4a640de65b1326181bd37ac836
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-30 09:48:11 +0100

    Refactor Auth class methods to handle customer creation and login with input parsing and improved error responses

--- Auth.php
+++ Auth.php
@@ -1,60 +1,68 @@
 <?php
-    require('Config.php');
-    class Auth extends Config {
-        public function createCustomer($first_name, $last_name, $email, $password) {
-            // Hash password
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+require('Config.php');
+class Auth extends Config
+{
+    public function createCustomer($first_name, $last_name, $email, $password)
+    {
+        $input = file_get_contents('php://input');
+        $customerDetails = json_decode($input);
+        $first_name = $customerDetails->first_name;
+        $last_name = $customerDetails->last_name;
+        $email = $customerDetails->email;
+        $password = $customerDetails->password;
+        // Hash password
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-            $confirmQuery = "SELECT * FROM customer_tb WHERE email='$email'";
-            $result = mysqli_query($this->connection, $confirmQuery);
-            // print_r($result);
-            if(mysqli_num_rows($result) > 0) {
-                echo json_encode(['status' => 400, 'message' => 'Account already exist']);
+        $confirmQuery = "SELECT * FROM customer_tb WHERE email='$email'";
+        $result = mysqli_query($this->connection, $confirmQuery);
+        // print_r($result);
+        if (mysqli_num_rows($result) > 0) {
+            echo json_encode(['status' => 400, 'message' => 'Account already exist']);
+        } else {
+            $query = "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`) VALUES('$first_name', '$last_name', '$email', '$hashedPassword')";
+            $saveCustomer = mysqli_query($this->connection, $query);
+            if ($saveCustomer) {
+                echo json_encode(['status' => 200, 'message' => 'customer sign up successful']);
             } else {
-                $query = "INSERT INTO `customer_tb`(`first_name`, `last_name`, `email`, `password`) VALUES('$first_name', '$last_name', '$email', '$hashedPassword')";
-                $saveCustomer = mysqli_query($this->connection, $query);
-                if($saveCustomer) {
-                    echo json_encode(['status' => 200, 'message' => 'customer sign up successful']);
-                } else {
-                    echo json_encode(['status' => 500, 'message' => 'customer creation failed']);
-                }
-            }
-        }
-
-        public function loginCustomer($email, $password) {
-            $query = "SELECT * FROM customer_tb WHERE email='$email'";
-            $result = mysqli_query($this->connection, $query);
-            $foundUser = mysqli_fetch_assoc($result);
-            // print_r($foundUser);
-
-            if(mysqli_num_rows($result) > 0) {
-                $verifiedUser = password_verify($password, $foundUser['password']);
-                // echo $verifiedUser;
-                if ($verifiedUser) {
-                    echo json_encode(['status' => 200, 'message' => 'Login successful']);
-                } else {
-                    echo json_encode(['status' => 400, 'message' => 'Email or password is incorrect']);
-                }
-            } else {
-                echo json_encode(['status' => 400, 'message' => 'account does not exist. Kindly sign up first']);
+                echo json_encode(['status' => 500, 'message' => 'customer creation failed']);
             }
         }
     }
 
-    // Receive from frontend
-    $input = file_get_contents('php://input');
-    $customerDetails = json_decode($input); // COnverts the details into what PHP understands
-    $first_name = $customerDetails->first_name;
-    $last_name = $customerDetails->last_name;
-    $email = $customerDetails->email;
-    $password = $customerDetails->password;
+    public function loginCustomer($email, $password)
+    {
+        $query = "SELECT * FROM customer_tb WHERE email='$email'";
+        $result = mysqli_query($this->connection, $query);
+        $foundUser = mysqli_fetch_assoc($result);
+        // print_r($foundUser);
 
-    $auth = new Auth;
-    if ($first_name && $last_name) {
-        $auth->createCustomer($first_name, $last_name, $email, $password);
-    } else {
-        $auth->loginCustomer($email, $password);
+        if (mysqli_num_rows($result) > 0) {
+            $verifiedUser = password_verify($password, $foundUser['password']);
+            // echo $verifiedUser;
+            if ($verifiedUser) {
+                echo json_encode(['status' => 200, 'message' => 'Login successful']);
+            } else {
+                echo json_encode(['status' => 400, 'message' => 'Email or password is incorrect']);
+            }
+        } else {
+            echo json_encode(['status' => 400, 'message' => 'account does not exist. Kindly sign up first']);
+        }
     }
-    // $auth->createCustomer('Femi', 'John', 'fm@gmail.com', '123456')
-    // $auth->loginCustomer('roma@mailinator.com', 'Femi1234$')
-?>
+}
+
+// Receive from frontend
+// $input = file_get_contents('php://input');
+// $customerDetails = json_decode($input); // COnverts the details into what PHP understands
+// $first_name = $customerDetails->first_name;
+// $last_name = $customerDetails->last_name;
+// $email = $customerDetails->email;
+// $password = $customerDetails->password;
+
+// $auth = new Auth;
+// if ($first_name && $last_name) {
+//     $auth->createCustomer($first_name, $last_name, $email, $password);
+// } else {
+//     $auth->loginCustomer($email, $password);
+// }
+// $auth->createCustomer('Femi', 'John', 'fm@gmail.com', '123456')
+// $auth->loginCustomer('roma@mailinator.com', 'Femi1234$')
