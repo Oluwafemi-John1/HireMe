63ef5aa6aa83ff2d3c25e9b94ac144164be3987d
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-10-30 08:32:58 +0100

    Add Router class with methods for route registration and request processing

---
+++ Router.php
@@ -0,0 +1,11 @@
+<?php
+
+class Router {
+    // Register routes
+    public function add() {}
+
+    // Process all route requests from the frontend
+    public function dispatch() {
+        
+    }
+}
